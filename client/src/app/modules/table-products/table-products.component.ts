a05892e32d8891deb66e4f399059cd5f578c07c9
Jordan Mor <jordanmor@gmail.com>
2019-09-23 13:35:35 -0400

    [CLIENT] Add pagination functionality
    Includes ability to go to a clicked page number, as well as the next
    or prev page. Current page highlighted.
    Pagination works with all sorted columns.
    Next button disabled when last page reached
    Prev button disabled when first page reached

--- client/src/app/modules/table-products/table-products.component.ts
+++ client/src/app/modules/table-products/table-products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../core/http/api.service';
 import { Product } from '../../core/models/product';
-import { range, round } from 'lodash';
+import { range } from 'lodash';
 
 @Component({
   selector: 'app-table-products',
@@ -14,30 +14,48 @@ export class TableProductsComponent implements OnInit {
   products: Array<Product>;
   pages: Array<number> = [];
   currentPage: number;
-  round: any = round;
-  isSortDirectionDefault: boolean = true;
+  isSortDirectionAsc: boolean = true;
   sortBy: string = 'productId';
+  orderByDiscount: boolean = false;
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
-    this.showProducts(0, this.isSortDirectionDefault, this.sortBy);
+    this.showProducts(0, this.isSortDirectionAsc, this.sortBy);
   }
 
-  sortProducts(sortBy: string) {
-    this.isSortDirectionDefault = !this.isSortDirectionDefault;
-    this.sortBy = sortBy;
-    this.showProducts(0, this.isSortDirectionDefault, this.sortBy);
+  goToPage(page: number) {
+    this.showProductsSortedBy(this.sortBy, page);
   }
 
-  orderProductsByDiscount() {
-    this.isSortDirectionDefault = !this.isSortDirectionDefault;
-    this.showProducts(0, this.isSortDirectionDefault, this.sortBy, true);
+  prevPage() {
+    this.currentPage -= 1
+    this.showProductsSortedBy(this.sortBy, this.currentPage);
   }
 
-  showProducts(page?: number, isSortDirectionDefault?: boolean, sortBy?: string, orderByDiscount?: boolean) {
+  nextPage() {
+    this.currentPage += 1
+    this.showProductsSortedBy(this.sortBy, this.currentPage);
+  }
+
+  showProductsSortedBy(sortBy: string, page: number = 0) {
+    if(page === 0) {
+      this.isSortDirectionAsc = !this.isSortDirectionAsc;
+      this.sortBy = sortBy;
+    } else {
+      page -= 1;
+    }
+    if(sortBy === 'discount') {
+      this.showProducts(page, this.isSortDirectionAsc, this.sortBy, true);
+    } else {
+      this.orderByDiscount = false;
+      this.showProducts(page, this.isSortDirectionAsc, this.sortBy);
+    }
+  }
+
+  showProducts(page: number, isSortDirectionAsc: boolean, sortBy: string, orderByDiscount: boolean =  false) {
     let direction: string;
-    if (isSortDirectionDefault) {
+    if (isSortDirectionAsc) {
       direction = 'ASC';
     } else {
       direction = 'DESC';
