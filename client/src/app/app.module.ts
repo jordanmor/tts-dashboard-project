ed3b8db0fa5a797ca06e4529be1fafff0a9ac065
Jordan Mor <jordanmor@gmail.com>
2019-09-23 15:53:42 -0400

    [CLIENT] Install ng-bootstrap for Angular
    Replace boostrap CDNs with ng-bootstrap for pagination component
    and other features made to work better with Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
@@ -24,7 +25,8 @@ import { PaginationComponent } from './shared/pagination/pagination.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
