82b9b75e9f9ea86d1395f6ef2749f501f96d5854
Jordan Mor <jordanmor@gmail.com>
2019-10-02 06:13:27 -0400

    [CLIENT] Put filter into it's own component

---
+++ client/src/app/modules/filter/filter.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Category } from 'src/app/core/models/category';
+import { Supplier } from 'src/app/core/models/supplier';
+import { FilterData } from 'src/app/core/models/filterData';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.css']
+})
+export class FilterComponent implements OnInit {
+
+  @Input() categories: Category[];
+  @Input() suppliers: Supplier[];
+  @Input() datasetName: string;
+  @Output() onFilteredRequest = new EventEmitter();
+  filterOn: boolean = false;
+  filterBy: string;
+  filterByValue: any;
+  filterAlsoBy: string;
+  filterAlsoByValue: any;
+  filterForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createFilterForm();
+   }
+
+  ngOnInit() {
+    this.filterBy = '';
+    this.filterByValue = '';
+    this.filterAlsoBy = 'none';
+  }
+
+  filteredRequest(filterData: any) {
+    this.onFilteredRequest.emit(filterData);
+  }
+
+  setFilterBy($event: any) {
+    this.filterAlsoBy = 'none'
+    this.filterByValue = '';
+    this.filterBy = $event.target.value;
+  }
+
+  setFilterByValue($event: any) {
+    this.filterByValue = $event.target.value;
+    if(this.filterOn) {
+      console.log(this.filterForm.value);
+    }
+  }
+
+  setSecondFilterBy($event: any) {
+    this.filterAlsoBy = $event.target.value;
+  }
+
+  createFilterForm() {
+    this.filterForm = this.formBuilder.group({
+      filterBy: '',
+      filterByValue: '',
+      filterAlsoBy: 'none',
+      filterAlsoByValue: ''
+    });
+  }
+
+  reset() {
+    this.filterBy = '';
+    this.filterByValue = '';
+    this.filterAlsoBy = 'none';
+    this.createFilterForm();
+  }
+
+  onSubmitFilterForm() {
+    let filterData: FilterData = new FilterData();
+    this.filterOn = !this.filterOn; // toggle filter
+    filterData.filtered = this.filterOn;
+    if(!this.filterOn) {
+      this.reset();
+      this.filteredRequest(filterData);
+    } else {
+      this.setFilterData();
+      filterData.filterBy = `${this.filterBy}+${this.filterByValue}`;
+      filterData.filterAlsoBy = `${this.filterAlsoBy}+${this.filterAlsoByValue}`;
+      filterData.filterName1 = this.filterBy;
+      filterData.filterName2 = this.filterAlsoBy;
+      this.filteredRequest(filterData);
+    }
+  }
+
+  createFilteredRequest() {
+
+  }
+
+  setFilterData() {
+    this.filterBy = this.filterForm.get('filterBy').value;
+    this.filterByValue = this.filterForm.get('filterByValue').value;
+    this.filterAlsoByValue = this.filterForm.get('filterAlsoByValue').value;
+    this.filterAlsoBy = this.filterForm.get('filterAlsoBy').value;
+    // Handles submission of filter with availability pre-checked
+    if(this.filterBy === 'availability' && this.filterByValue === '') {
+      this.filterByValue = true;
+    }
+    // Handles submission of filter with availability pre-checked
+    if(this.filterAlsoBy === 'availability' && this.filterAlsoByValue === '') {
+      this.filterAlsoByValue = true;
+    }
+  }
+
+}
--- client/src/app/modules/main-page-header/main-page-header.component.ts
+++ client/src/app/modules/main-page-header/main-page-header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
 import { Category } from 'src/app/core/models/category';
 import { Supplier } from 'src/app/core/models/supplier';
 import { FilterData } from '../../core/models/filterData';
@@ -20,21 +19,11 @@ export class MainPageHeaderComponent implements OnInit {
   @Input() suppliers: Supplier[];
   @Output() onModalFormSubmit = new EventEmitter();
   @Output() onFilteredRequest = new EventEmitter();
-  filterOn: boolean = false;
-  filterBy: string;
-  filterByValue: any;
-  filterAlsoBy: string;
-  filterAlsoByValue: any;
-  filterForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.createFilterForm();
-   }
+  constructor() { }
 
   ngOnInit() {
-    this.filterBy = '';
-    this.filterByValue = '';
-    this.filterAlsoBy = 'none';
+
   }
 
   hideAlert() {
@@ -47,66 +36,5 @@ export class MainPageHeaderComponent implements OnInit {
 
   filteredRequest(filterData: any) {
     this.onFilteredRequest.emit(filterData);
-  }
-
-  setFilterBy($event: any) {
-    this.filterAlsoBy = 'none'
-    this.filterByValue = '';
-    this.filterBy = $event.target.value;
-  }
-
-  setFilterByValue($event: any) {
-    this.filterByValue = $event.target.value;
-  }
-
-  setSecondFilterBy($event: any) {
-    this.filterAlsoBy = $event.target.value;
-  }
-
-  createFilterForm() {
-    this.filterForm = this.formBuilder.group({
-      filterBy: '',
-      filterByValue: '',
-      filterAlsoBy: 'none',
-      filterAlsoByValue: ''
-    });
-  }
-
-  reset() {
-    this.filterBy = '';
-    this.filterByValue = '';
-    this.filterAlsoBy = 'none';
-    this.createFilterForm();
-  }
-
-  onSubmitFilterForm() {
-    let filterData: FilterData = new FilterData();
-    this.filterOn = !this.filterOn; // toggle filter
-    filterData.filtered = this.filterOn;
-    if(!this.filterOn) {
-      this.reset();
-    } else {
-      this.setFilterData();
-      filterData.filterBy = `${this.filterBy}+${this.filterByValue}`;
-      filterData.filterAlsoBy = `${this.filterAlsoBy}+${this.filterAlsoByValue}`;
-      filterData.filterName1 = this.filterBy;
-      filterData.filterName2 = this.filterAlsoBy;
-    }
-    this.filteredRequest(filterData);
-  }
-
-  setFilterData() {
-    this.filterBy = this.filterForm.get('filterBy').value;
-    this.filterByValue = this.filterForm.get('filterByValue').value;
-    this.filterAlsoByValue = this.filterForm.get('filterAlsoByValue').value;
-    this.filterAlsoBy = this.filterForm.get('filterAlsoBy').value;
-    // Handles submission of filter with availability pre-checked
-    if(this.filterBy === 'availability' && this.filterByValue === '') {
-      this.filterByValue = true;
-    }
-    // Handles submission of filter with availability pre-checked
-    if(this.filterAlsoBy === 'availability' && this.filterAlsoByValue === '') {
-      this.filterAlsoByValue = true;
-    }
   }
 }
