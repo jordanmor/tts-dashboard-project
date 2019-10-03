7f6dcaa711cf5487332f802b178ca3f753eff85e
Jordan Mor <jordanmor@gmail.com>
2019-10-03 18:27:28 -0400

    [CLIENT] - Fix multiple filter bugs
    Bugs related to changing values while the filter is on and
    receiving the correct response data, as well as the correct
    selections displayed according to the other selected filter

--- client/src/app/modules/filter/filter.component.ts
+++ client/src/app/modules/filter/filter.component.ts
@@ -27,64 +27,17 @@ export class FilterComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.filterBy = '';
-    this.filterByValue = '';
-    this.filterAlsoBy = 'none';
+    this.resetFilter();
   }
 
-  filteredRequest(filterData: any) {
-    this.onFilteredRequest.emit(filterData);
-  }
+    // ------- FILTER TOGGLE ------- //
 
-  setFilterBy($event: any) {
-    this.filterAlsoBy = 'none'
-    this.filterByValue = '';
-    this.filterBy = $event.target.value;
-  }
-
-  setFilterByValue($event: any) {
-    this.filterByValue = $event.target.value;
-    if(this.filterOn) {
-      console.log(this.filterForm.value);
+    toggleFilter() {
+      this.filterOn = !this.filterOn; // toggle filter
+      this.createFilterRequest();
     }
-  }
 
-  setSecondFilterBy($event: any) {
-    this.filterAlsoBy = $event.target.value;
-  }
-
-  createFilterForm() {
-    this.filterForm = this.formBuilder.group({
-      filterBy: '',
-      filterByValue: '',
-      filterAlsoBy: 'none',
-      filterAlsoByValue: ''
-    });
-  }
-
-  reset() {
-    this.filterBy = '';
-    this.filterByValue = '';
-    this.filterAlsoBy = 'none';
-    this.createFilterForm();
-  }
-
-  onSubmitFilterForm() {
-    let filterData: FilterData = new FilterData();
-    this.filterOn = !this.filterOn; // toggle filter
-    filterData.filtered = this.filterOn;
-    if(!this.filterOn) {
-      this.reset();
-      this.filteredRequest(filterData);
-    } else {
-      this.setFilterData();
-      filterData.filterBy = `${this.filterBy}+${this.filterByValue}`;
-      filterData.filterAlsoBy = this.filterAlsoByValue === '' ? this.filterAlsoBy : `${this.filterAlsoBy}+${this.filterAlsoByValue}`;
-      filterData.filterName1 = this.filterBy;
-      filterData.filterName2 = this.filterAlsoBy;
-      this.filteredRequest(filterData);
-    }
-  }
+  // ------- FILTER REQUEST ------- //
 
   setFilterData() {
     this.filterBy = this.filterForm.get('filterBy').value;
@@ -99,6 +52,98 @@ export class FilterComponent implements OnInit {
     if(this.filterAlsoBy === 'availability' && this.filterAlsoByValue === '') {
       this.filterAlsoByValue = true;
     }
+  }
+
+  createFilterRequest() {
+    let filterData: FilterData = new FilterData();
+    filterData.filtered = this.filterOn;
+    if(!this.filterOn) {
+      this.resetFilter();
+      this.createFilterForm();
+      this.sendFilteredRequest(filterData);
+    } else {
+      this.setFilterData();
+      filterData.filterBy = `${this.filterBy}+${this.filterByValue}`;
+      filterData.filterAlsoBy = this.filterAlsoByValue === '' ? this.filterAlsoBy : `${this.filterAlsoBy}+${this.filterAlsoByValue}`;
+      filterData.filterName1 = this.filterBy;
+      filterData.filterName2 = this.filterAlsoBy;
+      this.sendFilteredRequest(filterData);
+    }
+  }
+
+  sendFilteredRequest(filterData: any) {
+    this.onFilteredRequest.emit(filterData);
+  }
+
+  // -------- FILTER FORM --------- //
+
+  createFilterForm() {
+    this.filterForm = this.formBuilder.group({
+      filterBy: '',
+      filterByValue: '',
+      filterAlsoBy: 'none',
+      filterAlsoByValue: ''
+    });
+  }
+
+  updateFilterFormValues() {
+    const newFormData = {
+      filterBy: this.filterBy,
+      filterByValue: this.filterByValue,
+      filterAlsoBy: this.filterAlsoBy,
+      filterAlsoByValue: this.filterAlsoByValue
+    }
+    this.filterForm.patchValue(newFormData);
+  }
+
+  // -- SET FILTERS AND FILTER VALUES  -- //
+
+  setFilterBy($event: any) {
+    this.resetFilter();
+    this.filterBy = $event.target.value;
+
+    if(this.filterOn) {
+      if(this.filterBy === 'availability') {
+        this.filterByValue = true;
+        this.updateFilterFormValues();
+        this.createFilterRequest();
+      }
+    }
+  }
+
+  setFilterByValue($event: any) {
+    this.filterByValue = $event.target.value;
+    if(this.filterOn) {
+      this.createFilterRequest();
+    }
+  }
+
+  setFilterAlsoBy($event: any) {
+    this.filterAlsoBy = $event.target.value;
+    this.filterAlsoByValue = '';
+    if(this.filterOn) {
+      if(this.filterAlsoBy === 'availability') {
+        this.filterAlsoByValue = true;
+        this.updateFilterFormValues();
+        this.createFilterRequest();
+      } else if(this.filterAlsoBy === 'none')
+          this.updateFilterFormValues();
+          this.createFilterRequest();
+    }
+  }
+
+  setFilterAlsoByValue($event: any) {
+    this.filterAlsoByValue = $event.target.value;
+    if(this.filterOn) {
+      this.createFilterRequest();
+    }
+  }
+
+  resetFilter() {
+    this.filterBy = '';
+    this.filterByValue = '';
+    this.filterAlsoBy = 'none';
+    this.filterAlsoByValue = '';
   }
 
 }
