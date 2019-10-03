4477e846cf4cbba3bd956663390b0ca676029b53
Jordan Mor <jordanmor@gmail.com>
2019-10-03 11:22:10 -0400

    [CLIENT] Go to prev page after last page item deleted
    When last item is removed from a paginated page, the request for
    updated data will contain a call for the prev paginated page.
    The paginatedResponse model now includes the numberOfElements of the
    current paginated page so app can check when the last page item
    is removed

--- client/src/app/core/models/paginatedResponse.ts
+++ client/src/app/core/models/paginatedResponse.ts
@@ -1,6 +1,9 @@
 export class PaginatedResponse {
   content: Array<any>;
   number: number; // Current page number
+  // All relevant data items in database
   totalElements: number;
+  // Number of data elements in current paginated response
+  numberOfElements: number;
   size: number; // Page size
 }
